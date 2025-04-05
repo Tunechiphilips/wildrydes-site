160ac25a1dbbae48e469cfe13e74a7b226a42ef3
Tunechiphilips <sylvesterebisike@gmail.com>
2025-04-05 00:30:47 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_V808CwBoU', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '131lunr6hpgplmqe8fkhu6kkih', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_9T2QnUAa7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3npa8kblufq61qtg93oavbh8b1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'us-east-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
