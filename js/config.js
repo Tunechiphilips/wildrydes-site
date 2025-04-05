34ebc542f1d7af260a6caec421963ef9cb76f8e2
Tunechiphilips <sylvesterebisike@gmail.com>
2025-04-05 00:44:27 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_9T2QnUAa7', // e.g. us-east-2_uXboG5pAb
+        userPoolId: '3npa8kblufq61qtg93oavbh8b1', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3npa8kblufq61qtg93oavbh8b1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
